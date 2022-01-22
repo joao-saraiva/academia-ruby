7655c9d73a1dac2e6ef5fb0f85b8c155ff9df787
Joao Victor <joao_saraiva@edu.unifor.br>
2022-01-22 13:55:28 -0300

    configurated shoulda matchers and created rspec tests for users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  [:email, :password, :password_confirmation, :cpf].each do |attribute| 
+    it { is_expected.to  validate_presence_of(attribute) }
+  end
 end
