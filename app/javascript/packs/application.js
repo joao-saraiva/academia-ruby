7f6591d2e27efd1b05ccef4c72d800890659ad2d
Joao Victor <joao_saraiva@edu.unifor.br>
2022-01-29 01:54:35 -0300

    starting the include of the theme

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,21 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+require("@rails/ujs").start()
+require("@rails/activestorage").start()
+require("channels")
+require("jquery")
+require('popper.js')
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+require("bootstrap")
+require.context("../img", true)
+import '../css/site.css';
+//import "../stylesheets/application";
+import "../stylesheets/main";
+
+document.addEventListener("turbolinks:load", function() {
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+        $('[data-toggle="popover"]').popover()
+    })
+})
