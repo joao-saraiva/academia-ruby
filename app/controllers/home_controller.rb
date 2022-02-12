b8c0da4c863c2079e33d2f2949d259bc5b3e257e
Joao Victor <joao_saraiva@edu.unifor.br>
2022-02-11 22:57:31 -0300

    AR#15 criado a parte sobre da aplicacao

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,20 @@
 class HomeController < ApplicationController
-
+    layout :resolve_layout
     def index
+    end
+
+    def about
+        #format.html { render :about, layout: 'other_pages_layout'}
+    end
+
+    private
+
+    def resolve_layout
+        case action_name
+        when 'index'
+            'application'
+        else 'about'
+            'other_pages_layout'
+        end
     end
 end
